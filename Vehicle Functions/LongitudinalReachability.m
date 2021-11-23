e410a686a03f0a374e3a26beee022e0b530d545b
Manuel Schirmer <manuel.schirmer@mailbox.tu-dresden.de>
2021-11-23 13:30:27 +0100

    Updated structure to integrate reachability analysis to ego vehicle.

--- Vehicle Functions/LongitudinalReachability.m
+++ Vehicle Functions/LongitudinalReachability.m
@@ -1,21 +1,8 @@
-classdef LongitudinalReachability < matlab.System
+classdef LongitudinalReachability < ReachabilityAnalysis
 % Longitudinal reachability with constant (minimum/maximum) acceleration assumption
-
-     properties(Nontunable)
-        minimumAcceleration % Minimum longitudinal acceleration
-        maximumAcceleration % Maximum longitudinal acceleration
-         
-        timeHorizon % Time horizon for trajectory genereation [s]
-        Ts % Sampling time for trajectory generation [s]
-    end
 
     % Pre-computed constants
     properties(Access = private)
-        k % Number of discrete time steps
-        
-        A_prime % Modified A-Matrix for reachability analysis
-        B_prime % Modified B-Matrix for reachability analysis
-
         counter % Counter to stop at correct simulation time
         futureStatePrediction % Store future state predictions for verification
         err_s_v % Error between predicted s and actual s and predicted velocity and actual velocity [t, deltaS, deltaV]
@@ -24,11 +11,7 @@ classdef LongitudinalReachability < matlab.System
     methods(Access = protected)
         function setupImpl(obj)
             % Perform one-time calculations, such as computing constants
-            
-            obj.k = obj.timeHorizon/obj.Ts - 1; % -1 because state(k+1) should be equal to time horizon
-            
-            obj.A_prime = obj.calculateAPrime(obj.k);
-            obj.B_prime = obj.calculateBPrime(obj.k);
+            setupImpl@ReachabilityAnalysis(obj) 
 
             obj.counter = 0;
             obj.futureStatePrediction = [];
@@ -36,26 +19,15 @@ classdef LongitudinalReachability < matlab.System
             obj.err_s_v = [time, zeros(length(time), 2)];
         end
 
-        function [sFuture_min, sFuture_max] = stepImpl(obj, velocity_0, s_0)
+        function [sFuture_min, sFuture_max] = stepImpl(obj, v_0, s_0)
         % Return minimum and maximum predicted future s
             
-            initialState = [s_0; velocity_0];
-                    
-            futureState_min = obj.A_prime*initialState + obj.B_prime*obj.minimumAcceleration;
-            if futureState_min(2) < 0
-                % Account for the -speed error in the prediction and
-                % correct the position value and set predicted speed to 0
-                
-                futureState_min(2) = 0;
-                t_stop = -velocity_0/obj.minimumAcceleration; % v(t) = 0 = acc*t + v_0 if acc = const.
-                futureState_min(1) = s_0 + velocity_0*t_stop/2;
-            end
-            sFuture_min = futureState_min(1);
+            [futureState_min, futureState_max] = obj.predictLongitudinalFutureState(s_0, v_0);
             
-            futureState_max = obj.A_prime*initialState + obj.B_prime*obj.maximumAcceleration;
+            sFuture_min = futureState_min(1);
             sFuture_max = futureState_max(1);
             
-            obj.verifyReachabilityAnalysis(initialState);
+            obj.verifyReachabilityAnalysis([s_0; v_0]);
         end
 
         function verifyReachabilityAnalysis(obj, initialState)
@@ -71,20 +43,6 @@ classdef LongitudinalReachability < matlab.System
                 obj.futureStatePrediction = obj.A_prime*initialState + obj.B_prime*aLead_ref;
                 obj.counter = obj.counter + 1; 
             end
-        end
-
-        function A_prime = calculateAPrime(obj, k)
-        % Calculate modified A-Matrix for k time steps
-
-            A_prime = [1, (k+1)*obj.Ts; 
-                       0,       1      ];
-        end
-
-        function B_prime = calculateBPrime(obj, k)
-        % Calculate modified B-Matrix for k time steps
-
-            B_prime = [obj.Ts^2/2*sum(1:2:(2*k+1)); 
-                            obj.Ts*(k+1)           ];
         end
     
         function [out1, out2] = getOutputSizeImpl(~)
---
+++ Vehicle Functions/ReachabilityAnalysis.m
@@ -0,0 +1,86 @@
+classdef ReachabilityAnalysis < matlab.System & handle & matlab.system.mixin.Propagates & matlab.system.mixin.SampleTime & matlab.system.mixin.CustomIcon
+% Superclass for reachability analysis
+
+     properties(Nontunable)
+        minimumAcceleration % Minimum longitudinal acceleration
+        maximumAcceleration % Maximum longitudinal acceleration
+        
+        wheelBase % Wheel base vehicle [m]
+        steerAngle_max % Maximum steering angle [rad]
+         
+        timeHorizon % Time horizon for trajectory genereation [s]
+        Ts % Sampling time for trajectory generation [s]
+    end
+
+    % Pre-computed constants
+    properties(Access = protected)
+        k % Number of discrete time steps according to specified time horizon
+        
+        A_prime % Modified A-Matrix for reachability analysis
+        B_prime % Modified B-Matrix for reachability analysis
+    end
+
+    methods(Access = protected)
+        function setupImpl(obj)
+            % Perform one-time calculations, such as computing constants
+            
+            obj.k = obj.timeHorizon/obj.Ts - 1; % -1 because state(k+1) should be equal to time horizon
+            
+            obj.A_prime = obj.calculateAPrime(obj.k);
+            obj.B_prime = obj.calculateBPrime(obj.k);
+        end
+        
+        function [futureState_min, futureState_max] = predictLongitudinalFutureState(obj, s_0, v_0)
+        % Predict future state (longitudinal displacement, longitudinal velocity) according to 
+        % longitudinal reachability analysis for a constant minimum and maximum acceleration
+            
+            initialState = [s_0; v_0];
+        
+            futureState_min = obj.A_prime*initialState + obj.B_prime*obj.minimumAcceleration;
+            if futureState_min(2) < 0
+                % Account for the -speed error in the prediction and
+                % correct the position value and set predicted speed to 0
+                
+                futureState_min(2) = 0;
+                t_stop = -v_0/obj.minimumAcceleration; % v(t) = 0 = acc*t + v_0 if acc = const.
+                futureState_min(1) = s_0 + v_0*t_stop/2;
+            end
+            
+            futureState_max = obj.A_prime*initialState + obj.B_prime*obj.maximumAcceleration;
+        end
+        
+        function steeringReachability = calculateSteeringReachability(obj, pose, v_constant)
+        % Calcuate reachability for all possible steering angles
+            
+            steeringAngles = -obj.steerAngle_max:0.01:obj.steerAngle_max;
+            arcLength = v_constant*obj.timeHorizon; % Only for constant velocities
+            turningRadius = obj.wheelBase./tan(steeringAngles);
+            arcAngle = arcLength./turningRadius;
+            
+            x_destination = pose(1) + turningRadius.*(sin(pose(3) + arcAngle) - sin(pose(3)));
+            y_destination = pose(2) + turningRadius.*(cos(pose(3)) - cos(pose(3) + arcAngle));
+            
+            
+            steeringReachability = [x_destination', y_destination'];
+        end
+        
+%         function calculateSteeringAngleArc(obj, pose, steeringAngle)
+%         % Return tunring arc for a given steering angle, possible in specified time horizon
+%             
+%         end
+
+        function A_prime = calculateAPrime(obj, k)
+        % Calculate modified A-Matrix for constant accelerations and k time steps
+
+            A_prime = [1, (k+1)*obj.Ts; 
+                       0,       1      ];
+        end
+
+        function B_prime = calculateBPrime(obj, k)
+        % Calculate modified B-Matrix for constant accelerations and k time steps
+
+            B_prime = [obj.Ts^2/2*sum(1:2:(2*k+1)); 
+                            obj.Ts*(k+1)           ];
+        end
+    end
+end
