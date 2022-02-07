63e9766f11784449d318c778273ff03b7d796cff
Manuel Schirmer <manuel.schirmer@mailbox.tu-dresden.de>
2022-02-07 09:05:17 +0100

    Specifiy planner for driving scenarios.

--- DrivingScenarios.m
+++ DrivingScenarios.m
@@ -6,7 +6,7 @@
 % -> ToRightLane_FreeDrive -> RightLane_FreeDrive
 
 prepare_simulation('s_0', [0, 60, 40], 'd_0', [0, 0, 3.7], ...
-    'v_0', [20, 10, 13], 'v_ref', [20, 10, 13]);
+    'v_0', [20, 10, 13], 'v_ref', [20, 10, 13], 'planner', 'MANUAL');
 run_simulation();
 
 % Expected Output: 24.01.2022
@@ -27,7 +27,7 @@ run_simulation();
 % -> LeftLane_EmergencyBrake -> LeftLane_VehicleFollowing -> ...
 
 prepare_simulation('s_0', [0, 40, 20], 'd_0', [0, 0, 3.7], ...
-    'v_0', [23.5, 10, 30], 'v_ref', [30, 10, 4.2]);
+    'v_0', [23.5, 10, 30], 'v_ref', [30, 10, 4.2], 'planner', 'MANUAL');
 run_simulation();
 
 % Expected Output: 24.01.2022
@@ -57,7 +57,7 @@ run_simulation();
 % -> RightLane_VehicleFollowing -> RightLane_FreeDrive
 
 prepare_simulation('s_0', [0, 9, 0], 'd_0', [0, 0, 3.7], ...
-    'v_0', [15, 10, 30], 'v_ref', [15, 30, 0]);
+    'v_0', [15, 10, 30], 'v_ref', [15, 30, 0], 'planner', 'MANUAL');
 run_simulation();
 
 % Expected Output: 24.01.2022
@@ -72,7 +72,7 @@ run_simulation();
 % -> LeftLane_FreeDrive -> ToRightLane_FreeDrive -> ...
 
 prepare_simulation('s_0', [0, 30, 55], 'd_0', [0, 0, 3.7], ...
-    'v_0', [16.1, 10, 0.3], 'v_ref', [16.1, 10, 30]);
+    'v_0', [16.1, 10, 0.3], 'v_ref', [16.1, 10, 30], 'planner', 'MANUAL');
 run_simulation();
 
 % Expected Output: 24.01.2022
