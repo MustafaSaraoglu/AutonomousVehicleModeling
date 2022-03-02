126461a935b26f93d11f8c4c766681bddf0795e8
Manuel Schirmer <manuel.schirmer@mailbox.tu-dresden.de>
2022-03-02 17:01:06 +0100

    Discount factor and backpropagation.

--- Vehicle Functions/DecisionMaking/TreeSearch.m
+++ Vehicle Functions/DecisionMaking/TreeSearch.m
@@ -7,7 +7,9 @@ classdef TreeSearch
         
         searchDepth  % Depth for search algorithm
         depthBound % Maximum depth to search for in one iteration
-        safety_limit % Safety boundary for each depth/iteration
+        safety_limit % Safety boundary for each depth/iteration, only necessary if safety of depth d 
+                     % in any case is more important than safety of depth d+1
+        discount % Discount future safety levels
         
         DecisionGenerator % Generate decisions
     end
@@ -21,6 +23,8 @@ classdef TreeSearch
             obj.DecisionGenerator = DecisionGenerator;
            
             obj.searchDepth = 2;
+            obj.discount = 0.1; % Sf = Sf(d=1) + 0.1*Sf(d=2) + ...  + 0.1^(d_final-1)*Sf(d_final)
+                                % Decimal: Sf = Sf(d=1),Sf(d=2)Sf(d=3)...
         end
         
         function [bestDecision_total, dG_final] = iterativeMinimax(obj, state_Ego0, ...
@@ -32,7 +36,7 @@ classdef TreeSearch
             for depth = 1:obj.searchDepth % Iterative deepening
                 % Initialise digraph
                 ID_global = DigraphTree.getNewID(0);
-                initNode = DigraphTree.getNodeName(ID_global, [state_Ego0, states_Other0], 0);
+                initNode = DigraphTree.getNodeName(ID_global, [state_Ego0, states_Other0]);
                 dG_initial = DigraphTree.initialise(initNode, [1, 0, 1]);
 
                 obj.depthBound = depth; % Remember depth boundary for each iteration
@@ -42,7 +46,7 @@ classdef TreeSearch
                 beta_0.liveness = Inf;
 
                 % Best decisions iteratively
-                [bestDecision_iteration, value_iteration, dG_iteration] = ...
+                [bestDecision_iteration, value_iteration, dG_iteration, ~] = ...
                     obj.planMaxManeuver(state_Ego0, d_destination, states_Other0, alpha_0, ...
                                         beta_0, 0, dG_initial, ID_global, depth);
                 dG_final{depth} = dG_iteration;
@@ -56,7 +60,7 @@ classdef TreeSearch
             end
         end
         
-        function [decisionsNext_Ego, value_max, graph] = planMaxManeuver(obj, state_Ego, d_goal, states_Other, alpha, beta, parentSafety, graph, parentID, depth2go)
+        function [decisionsNext_Ego, value_max, graph, parentNode] = planMaxManeuver(obj, state_Ego, d_goal, states_Other, alpha, beta, parentSafety, graph, parentID, depth2go)
         % Plan and decide for a safe maneuver according to specified searching depth
             
             decisionMax_Ego = []; % Decision of current depth, that maximises future value
@@ -67,7 +71,7 @@ classdef TreeSearch
             depth2go = depth2go - 1;
             depthCurrent = obj.depthBound - depth2go;
             
-            parentNode = DigraphTree.getNodeName(parentID, [state_Ego, states_Other], parentSafety);
+            parentNode = DigraphTree.getNodeName(parentID, [state_Ego, states_Other]);
             maxNode = [];
             
             time_trajectory = 0:obj.Ts:obj.Th;
@@ -89,26 +93,29 @@ classdef TreeSearch
                 end
                 
                 TS_Ego = decision_Ego.TS;
-                safetyLevel = 0;
+                safety = 0;
                 for id_other = 1:length(decisions_Other)
                     decision_Other = decisions_Other(id_other);
                     TS_Other = decision_Other.TS;
                     [~, unsafeDiscreteStates] = CellChecker.isSafeTransitions(TS_Ego, TS_Other);
-
-                    safetyLevel = min(safetyLevel, -length(unsafeDiscreteStates));
                     
-                    if safetyLevel < obj.safety_limit(depthCurrent) % Better option available previously
+                    % TODO: Safety saturation [0, 9]?
+                    newSafety = ...
+                        parentSafety - obj.discount^(depthCurrent-1)*length(unsafeDiscreteStates);
+                    safety = min(safety, newSafety);
+                    
+                    if safety < obj.safety_limit(depthCurrent) % Better option available previously
                         % Remove unsafer decisions
                         decisions_Ego(id_decision) = [];
                         break
                     end
                 end
                 
-                if safetyLevel >= obj.safety_limit(depthCurrent) % Only consider safer/as safe decisions 
+                if safety >= obj.safety_limit(depthCurrent) % Only consider safer/as safe decisions 
                     % Add safe decision to digraph
                     [graph, childNode, childID] = DigraphTree.expand(graph, parentNode, ...
                         decision_Ego.futureState, decision_Ego.description, ...
-                        [0, 1, 0], [0, 1, 0], safetyLevel);
+                        [0, 1, 0], [0, 1, 0]);
                     
                     % Expand tree for future states of safes decisions
                     futureState_Ego = decision_Ego.futureState;
@@ -118,11 +125,24 @@ classdef TreeSearch
                         % No future decision for this depth
                         decisionsFuture_Ego = [];
                         % Combined value for liveness and safety
-                        value = obj.evaluate(safetyLevel, futureState_Ego);  
+                        value = obj.evaluate(safety, futureState_Ego); 
+                        
                         % Plot decisions of other vehicles at end of the tree
-                        [~, ~, graph] = obj.planMinManeuver(futureState_Ego, futureState_Ego.d, futureStatesCombinations_Other, alpha, beta, safetyLevel, graph, childID, depth2go);
+                        for id_statesOther = 1:size(futureStatesCombinations_Other, 1)
+                            futureStates_Other = futureStatesCombinations_Other(id_statesOther, :);
+
+                            % Add other vehicles' possible decisions to digraph
+                            [graph, leafNode, ~] = ...
+                                DigraphTree.expand(graph, childNode, ...
+                                [state_Ego, futureStates_Other], ...
+                                ['Combination', num2str(id_statesOther)], [1, 0, 0], [1, 0, 0]);
+                            [graph, ~] = DigraphTree.updateNodeValue(graph, leafNode, value);
+                        end
+                        
+                        % Update value in graph
+                        [graph, childNode] = DigraphTree.updateNodeValue(graph, childNode, value);
                     else
-                        [decisionsFuture_Ego, value, graph] = obj.planMinManeuver(futureState_Ego, futureState_Ego.d, futureStatesCombinations_Other, alpha, beta, safetyLevel, graph, childID, depth2go);
+                        [decisionsFuture_Ego, value, graph, childNode] = obj.planMinManeuver(futureState_Ego, futureState_Ego.d, futureStatesCombinations_Other, alpha, beta, safety, graph, childID, depth2go);
                     end
 
                     % Max behaviour: Ego vehicle
@@ -140,42 +160,33 @@ classdef TreeSearch
                 end
             end
             
-            % Higlight best node and edge in digraph
             if ~isempty(maxNode) 
-                graph = DigraphTree.changeNodeColor(graph, maxNode, [0, 1, 1]);
-                graph = DigraphTree.changeEdgeColor(graph, parentNode, maxNode, [0, 1, 1]);
+                [graph, parentNode] = ...
+                    DigraphTree.backpropagate(graph, maxNode, parentNode, value_max, [0, 1, 1]);
             end
             
             decisionsNext_Ego = [decisionMax_Ego; decisionsNext_Ego];
         end
         
-        function [decisionsNext_Ego, value_min, graph] = planMinManeuver(obj, state_Ego, d_futureGoal, stateCombinations_Other, alpha, beta, parentSafety, graph, parentID, depth2go)
+        function [decisionsNext_Ego, value_min, graph, parentNode] = planMinManeuver(obj, state_Ego, d_futureGoal, stateCombinations_Other, alpha, beta, parentSafety, graph, parentID, depth2go)
         % For other vehicles choose the action, which is considered the most unsafe
             
             decisionsNext_Ego = [];
             value_min.safety = Inf;
             value_min.liveness = Inf;
             
-            parentNode = DigraphTree.getNodeName(parentID, state_Ego, parentSafety);
+            parentNode = DigraphTree.getNodeName(parentID, state_Ego);
             minNode = [];
         
             for id_statesOther = 1:size(stateCombinations_Other, 1)
                 futureStates_Other = stateCombinations_Other(id_statesOther, :);
                 
                 % Add other vehicles' possible decisions to digraph
-                [graph, childNode, childID] = DigraphTree.expand(graph, parentNode, ...
+                [graph, ~, childID] = DigraphTree.expand(graph, parentNode, ...
                         [state_Ego, futureStates_Other], ['Combination', num2str(id_statesOther)], ...
-                        [1, 0, 0], [1, 0, 0], parentSafety);
-                
-                if depth2go == 0
-                    % Plot at end of the tree without calling planMaxManeuver again
-                    if id_statesOther == size(stateCombinations_Other, 1)
-                        return
-                    end
-                    continue
-                end
+                        [1, 0, 0], [1, 0, 0]);
                     
-                [decisionsFuture_Ego, value, graph] = obj.planMaxManeuver(state_Ego, d_futureGoal, futureStates_Other, alpha, beta, parentSafety, graph, childID, depth2go);
+                [decisionsFuture_Ego, value, graph, childNode] = obj.planMaxManeuver(state_Ego, d_futureGoal, futureStates_Other, alpha, beta, parentSafety, graph, childID, depth2go);
                 
                 % Min behaviour: Other vehicles
                 if isempty(decisionsFuture_Ego)
@@ -199,10 +210,9 @@ classdef TreeSearch
                 end
             end
             
-            % Higlight worst node and edge in digraph
             if ~isempty(minNode)
-                graph = DigraphTree.changeNodeColor(graph, minNode, [1, 0, 1]);
-                graph = DigraphTree.changeEdgeColor(graph, parentNode, minNode, [1, 0, 1]);
+                [graph, parentNode] = ...
+                    DigraphTree.backpropagate(graph, minNode, parentNode, value_min, [1, 0, 1]);
             end
         end
         
--- Visualisation/DigraphTree.m
+++ Visualisation/DigraphTree.m
@@ -11,12 +11,12 @@ classdef DigraphTree
             dG = addnode(dG, nodeProperties);
         end
         
-        function [dG, child, childID] = expand(dG, parent, state, edgeName, colorNode, colorEdge, safety)
+        function [dG, child, childID] = expand(dG, parent, state, edgeName, colorNode, colorEdge)
         % Expand the tree, add a new state node and connect it to its parent
             
             childID = DigraphTree.getNewID();
             
-            child = DigraphTree.getNodeName(childID, state, safety);
+            child = DigraphTree.getNodeName(childID, state);
             dG = DigraphTree.connect(dG, parent, child, edgeName, colorNode, colorEdge);
         end
         
@@ -28,6 +28,30 @@ classdef DigraphTree
 
             edgeProperties = table({edgeName}, {colorEdge}, 'VariableNames', {'Power', 'Color'});
             dG = addedge(dG, parent, child, edgeProperties);
+        end
+        
+        function [dG, parentNode] = backpropagate(dG, childNode, parentNode, value, colorHighlight)
+        % Higlight min/max child node and edge to child and update value of parent node in digraph
+        
+            dG = DigraphTree.changeNodeColor(dG, childNode, colorHighlight);
+            dG = DigraphTree.changeEdgeColor(dG, parentNode, childNode, colorHighlight);
+
+            [dG, parentNode] = DigraphTree.updateNodeValue(dG, parentNode, value);
+        end
+        
+        function [dG, updatedNode] = updateNodeValue(dG, node, value)
+        % Update a nodes value and change the node name accordingly
+        
+            updatedNode = [node, ', S_{f}:', num2str(value.safety), ...
+                           ', L_{v}:', num2str(round(value.liveness, 1))];
+            dG = DigraphTree.changeNodeName(dG, node, updatedNode);
+        end
+        
+        function dG = changeNodeName(dG, node, name)
+        % Change the color of an existing node
+            
+            id_node = findnode(dG, node);
+            dG.Nodes.Name{id_node} = name;
         end
         
         function dG = changeNodeColor(dG, node, color)
@@ -44,14 +68,24 @@ classdef DigraphTree
             dG.Edges.Color{id_Edge} = color;
         end
         
-        function nodeName = getNodeName(ID, state, safety)
-        % Get unique node name defined by its ID
+        function nodeName = getNodeName(ID, state)
+        % Get initial unique node name defined by its ID and state(s)
+            
+            nodeName = ['ID:', dec2hex(ID), ...
+                        ' s:', '[', regexprep(num2str(round([state.s], 1)), '\s+',' '), ']', ...
+                        ', d:', '[', regexprep(num2str(round([state.d], 1)), '\s+',' '), ']', ...
+                        ', v:', '[', regexprep(num2str(round([state.speed], 1)), '\s+',' '), ']'];
+        end
+        
+        function nodeName = getUpdatedNodeName(ID, state, value)
+        % Get updated unique node name containting information about its ID, state and value
             
             nodeName = ['ID:', dec2hex(ID), ...
                         ' s:', '[', regexprep(num2str(round([state.s], 1)), '\s+',' '), ']', ...
                         ', d:', '[', regexprep(num2str(round([state.d], 1)), '\s+',' '), ']', ...
                         ', v:', '[', regexprep(num2str(round([state.speed], 1)), '\s+',' '), ']', ...
-                        ', S_{f}:', num2str(round(safety, 1))];
+                        ', S_{f}:', num2str(value.safety), ...
+                        ', L_{v}:', num2str(round(value.liveness, 1))];
         end
         
         function ID_global = getNewID(init)
