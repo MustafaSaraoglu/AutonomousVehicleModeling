5f7baab356a26f90882ebfcc2d63d1612457ce5e
Manuel Schirmer <manuel.schirmer@mailbox.tu-dresden.de>
2022-03-07 11:40:57 +0100

    Trajectory as input.

--- Vehicle Functions/Helper Functions/Continuous2Discrete.m
+++ Vehicle Functions/Helper Functions/Continuous2Discrete.m
@@ -1,8 +1,8 @@
-function discreteCells = Continuous2Discrete(spaceDiscretisation, s, d, time)
-% Maps continuous (s, d)-tuple(s) to discrete rectangle cell(s) and stores the entrance and 
+function discreteCells = Continuous2Discrete(spaceDiscretisation, frenetTrajectory)
+% Maps continuous Frenet trajectory to discrete rectangle cell containing the entrance and 
 % exit time to each cell
 % discreteCell: [idxRow, idxColumn, time_enter, time_exit]
-    
+
     % Converting cell array to matrix makes it easier and faster
     spaceDiscretisationMatrix = cell2mat(spaceDiscretisation);
     
@@ -10,15 +10,16 @@ function discreteCells = Continuous2Discrete(spaceDiscretisation, s, d, time)
     columns = (reshape(spaceDiscretisationMatrix(2, :), 2, []))';
     
     % Preallocate: At most 4 cells can be occupied for one (s, d) tuple
-    discreteCells = zeros(4*length(s), 4); 
+    discreteCells = zeros(4*frenetTrajectory.length, 4); 
     idx = 1;
     % Store index of previous iteration to get exit time, 
     % which can first be known in the next iteration
     idx_prev = []; 
-    for i = 1:length(s)
+    for i = 1:frenetTrajectory.length
         % Find row and column to given (s, d) tuple
-        idxRow = (find(s(i)>=rows(:, 1) & s(i)<=rows(:, 2)))';
-        idxColumn = (find(d(i)>=columns(:, 1) & d(i)<=columns(:, 2)))';
+        idxRow = (find(frenetTrajectory.s(i)>=rows(:, 1) & frenetTrajectory.s(i)<=rows(:, 2)))';
+        idxColumn = (find(frenetTrajectory.d(i)>=columns(:, 1) & ...
+                     frenetTrajectory.d(i)<=columns(:, 2)))';
         
         % All combination are necessary to find the corrisponding discrete cells
         cells = (combvec(idxRow, idxColumn))';
@@ -33,9 +34,10 @@ function discreteCells = Continuous2Discrete(spaceDiscretisation, s, d, time)
         idx_next = idx + size(cells, 1);
         
         discreteCells(idx:idx_next-1, 1:2) = cells;
-        discreteCells(idx:idx_next-1, 3) = time(i); % Entrance time
+        discreteCells(idx:idx_next-1, 3) = frenetTrajectory.time(i); % Entrance time
         if ~isempty(idx_prev)
-            discreteCells(idx_prev, 4) = time(i); % Exit time is entrance time of next cell
+            discreteCells(idx_prev, 4) = frenetTrajectory.time(i); % Exit time is entrance time 
+                                                                   % of next cell
         end
         idx_prev = idx:idx_next-1;
             
