23bc17ef5c657005100dd682a86e5ba5c668547b
MustafaSaraoglu <51496368+MustafaSaraoglu@users.noreply.github.com>
2022-06-14 20:51:33 +0200

    Tree generation with dummy functions.

--- ManeuverPlanning/POMDP/Node.m
+++ ManeuverPlanning/POMDP/Node.m
@@ -8,22 +8,67 @@ classdef Node
         id
         state
         actions
+        targetNodeID
+        UnsafetyValue
     end
     
     methods
-        function obj = Node(sourceNodeID,sourceEdgeName,state,actions)
+        function obj = Node(sourceNodeID,sourceEdgeName,state,actions,UnsafetyValue)
             %NODE Construct an instance of this class
             obj.sourceNodeID = sourceNodeID;
             obj.sourceEdgeName = sourceEdgeName;
             obj.id = counter();
             obj.state = state;
-            obj.actions = actions; 
+            obj.actions = actions;
+            obj.UnsafetyValue = UnsafetyValue;
+            obj.targetNodeID = [];
         end
         
-        function childNode = expand(obj,maneuver)
-            newState= 404;
-            newActions= 405;
-            childNode = Node(obj.id, maneuver, newState, newActions);
+        function childNode = expand(obj,maneuver,deltaT)
+            
+            if strcmp(maneuver, 'FD')
+                speed_new = obj.state.speed +1;
+                s_new = obj.state.s + speed_new*deltaT;
+                d_new = obj.state.d;
+                orientation_new = obj.state.orientation;
+                
+            elseif strcmp(maneuver, 'EB')
+                speed_new = obj.state.speed -3;
+                s_new = obj.state.s + speed_new*deltaT;
+                d_new = obj.state.d;
+                orientation_new = obj.state.orientation;
+                
+            elseif strcmp(maneuver, 'LC')
+                speed_new = obj.state.speed;
+                s_new = obj.state.s + speed_new*deltaT;
+                if abs(obj.state.d)<0.05 % tolerance for d value
+                    d_new = 3.7;
+                elseif abs(obj.state.d)-3.7<0.05
+                    d_new = 0;                    
+                end
+                orientation_new = obj.state.orientation;
+            end
+            newState = obj.state;
+            newState.s = s_new;
+            newState.d = d_new;
+            newState.orientation = orientation_new;
+            newState.speed = speed_new;
+            newActions= {'FD','EB','LC'};
+            childNode = Node(obj.id, maneuver, newState, newActions,[]);
+        end
+        
+
+    end
+    
+    methods(Static)
+        
+        function plotTree(allNodes)
+            
+            s = [allNodes.sourceNodeID];
+            t = [allNodes.targetNodeID];
+            G = digraph(s,t);
+            plot(G,'Layout','layered');
+            
         end
     end
 end
--- ManeuverPlanning/POMDP/Pomdp.m
+++ ManeuverPlanning/POMDP/Pomdp.m
@@ -9,25 +9,75 @@ clear obj;
 counter(0);
 Maneuvers = {'FD','EB','LC'};
 
+UnSafetyValue=0;
 
 rootNodes = [];
-leafNodes = [];
-newleafNodes = [];
 %% POMDP Generation
 % Create root node
-leafNode = Node(0,0,currentState_Ego,Maneuvers);
+leafNode = Node([],0,currentState_Ego,Maneuvers,UnSafetyValue);
 
-for maneuver = Maneuvers
-    %expand the root node for each maneuever
-    newleafNode = leafNode.expand(maneuver);
-    newleafNodes = [newleafNodes newleafNode];
+leafNodes = [leafNode];
 
+deltaT = 2; % horizon 2 seconds
+for k = 1:4
+    
+    
+    
+    for leafNode = leafNodes
+         
+    newleafNodes = [];
+    
+    for maneuver = Maneuvers
+        %expand the root node for each maneuever
+        newleafNode = leafNode.expand(maneuver,deltaT);
+        leafNode.targetNodeID = [leafNode.targetNodeID newleafNode.id];
+        UnSafetyValue = [];
+        %Calculate unsafety value
+        for otherVehicle =1:length(currentStates_Other)
+            
+            % Calculate PDF of Other Vehicles
+            pdf_other = calculatePDFofOtherVehicles(currentStates_Other(otherVehicle),deltaT,k);
+            % UnsafetyValue = Area under curve
+            if abs(newleafNode.state.d - currentStates_Other(otherVehicle).d) < 0.02 % Tolerance value for "d"
+                UnSafetyValue_new = abs(pdf_other.cdf(newleafNode.state.s+2)-pdf_other.cdf(newleafNode.state.s-2)); % Size +-2 meters from the center               
+                UnSafetyValue = [UnSafetyValue UnSafetyValue_new];
+            else
+                UnSafetyValue_new = 0;
+                UnSafetyValue = [UnSafetyValue UnSafetyValue_new];
+            end
+            
+        end
+        newleafNode.UnsafetyValue = max(UnSafetyValue);
+        newleafNodes = [newleafNodes newleafNode];
+    end
+    
+    % Make leafNode a rootNode
+    rootNodes = [rootNodes leafNode];
+    
+    [val, idx] = intersect([leafNodes.id], [rootNodes.id]);
+    leafNodes(idx)=[]; % Remove the root node from the leaves
+    
+    leafNodes = [leafNodes newleafNodes];
+    
+    end
 end
 
-% Make leafNode a rootNode
-rootNodes = [rootNodes leafNode];
-leafNodes = [leafNodes newleafNodes];
+allNodes = [rootNodes leafNodes];
+
+Node.plotTree(allNodes);
+
+
 
 
 % Determine Rewards for children states
 % Make children states parent states and expand again
+
+
+function pd = calculatePDFofOtherVehicles(states,deltaT,k)
+    mean = states.s + states.speed*deltaT*k;
+    variance = deltaT*k;
+    range = [mean-(4*variance):0.1:mean+(4*variance)];
+    pd = makedist('Normal','mu',mean,'sigma',variance);
+    
+    %plot(range,pdf(pd,range));
+end
