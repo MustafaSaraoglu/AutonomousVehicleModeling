8bb03dfa6532cfd732abedb5dda9743f500a62c3
Mustafa Saraoğlu <51496368+MustafaSaraoglu@users.noreply.github.com>
2022-06-16 13:34:57 +0200

    Structured and cleaned the code a bit more. Still todos remaning.

--- ManeuverPlanning/POMDP/Node.m
+++ ManeuverPlanning/POMDP/Node.m
@@ -1,15 +1,14 @@
 classdef Node
-    %NODE Summary of this class goes here
-    %   Detailed explanation goes here
+    %NODE Every node of a tree contains the state/action information, source/target properties
     
     properties
-        sourceNodeID
-        sourceEdgeName
-        id
-        state
-        actions
-        targetNodeID
-        UnsafetyValue
+        sourceNodeID    % Parent Node id
+        sourceEdgeName  % Which action from parent node let to this state
+        id              % id of this node
+        state           % state information of this node
+        actions         % available actions (edges out) from this state
+        targetNodeID    % next states
+        UnsafetyValue   % Unsafety probability value 0<p<1
     end
     
     methods
@@ -17,15 +16,19 @@ classdef Node
             %NODE Construct an instance of this class
             obj.sourceNodeID = sourceNodeID;
             obj.sourceEdgeName = sourceEdgeName;
-            obj.id = counter();
+            obj.id = counter(); % assign id and increase the counter for the next possible nodes
             obj.state = state;
             obj.actions = actions;
             obj.UnsafetyValue = UnsafetyValue;
-            obj.targetNodeID = [];
+            obj.targetNodeID = []; % To be assigned if expanded
         end
         
         function childNode = expand(obj,maneuver,deltaT)
+            % Expand the node based on available actions (edges out)
+            %   Next states' values shall be calculated via reachability
+            %   based on the finite horizon
             
+            % TODO later: Remove hard-coded maneuvers
             if strcmp(maneuver, 'FD')
                 speed_new = obj.state.speed +1;
                 s_new = obj.state.s + speed_new*deltaT;
@@ -48,12 +51,17 @@ classdef Node
                 end
                 orientation_new = obj.state.orientation;
             end
-            newState = obj.state;
-            newState.s = s_new;
+            
+            
+            newState = obj.state; % Copy the object to create a state fast
+            % Assign newly calculated values for the time horizon
+            newState.s = s_new; 
             newState.d = d_new;
             newState.orientation = orientation_new;
             newState.speed = speed_new;
             newActions= {'FD','EB','LC'};
+            
+            % Return the newly created child node
             childNode = Node(obj.id, maneuver, newState, newActions,[]);
         end
         
@@ -62,29 +70,60 @@ classdef Node
     
     methods(Static)
         
-        function plotTree(allNodes)
-            
+        function plotTree(allNodes,leafNodes,unsafeBoundaryValue)
+            % Plot the tree using all the calculated nodes and edges
+            % creating a digraph: source node -> edge -> target node
             s = [allNodes.sourceNodeID];
-            t = [allNodes.targetNodeID];
-            
-            
-            
-            
+            t = [allNodes.targetNodeID];          
             G = digraph(s,t);
+            
+            % Change the labels of the digraph
             G.Edges.Labels =string([allNodes.sourceEdgeName])'; % Edge labels: Maneuvers
             G.Nodes.UnsafetyValues = [allNodes.UnsafetyValue]'; % Node labels: Unsafety values
             
             idx = [allNodes.id];
-            Pruned_idx= idx([allNodes.UnsafetyValue]>0.02);% Mark unsafe nodes Red
             Safest_idx = idx([allNodes.UnsafetyValue]<0.00001); % Mark safest nodes Green
             
+
+            
+            % Plot the tree from top down using 'Layout','layered'
             h=plot(G,'NodeLabel',G.Nodes.UnsafetyValues,'EdgeLabel',G.Edges.Labels,'Layout','layered');
             
-            
+            % Highlight Safe and Unsafe Nodes (Safe: Green, Unsafe: Red, InBetween: Blue)
             highlight(h,Safest_idx,'NodeColor','g')
             
+            % Optional: Change the Labels from just UnsafetyValues to Unsafety + Liveness values
+            NodeLabels = strcat('RLoU:',num2str(G.Nodes.UnsafetyValues,'%.4f'),'/ L:',num2str([vertcat([allNodes.state]).s]','%.1f'));
+            h.labelnode(idx,string(NodeLabels));    
+
+            % Highlight unsafe nodes with red
+            Pruned_idx = Node.findUnsafeNodes(allNodes,idx, unsafeBoundaryValue);
+            highlight(h,Pruned_idx,'NodeColor','r')    
+            
+            % Highlight edges that lead to unsafe nodes with red
+            unsafeSourceTargetPairs = Node.findUnsafeEdges(G,Pruned_idx);
+            highlight(h,unsafeSourceTargetPairs(:,1),unsafeSourceTargetPairs(:,2),'EdgeColor','r');
+            
+            % Find the safest state and track back to the initial state
+            [val,idx_safest] = min([leafNodes.UnsafetyValue]);
+            disp(['Safest state value: ',num2str(val)]);
+            SafestNode = leafNodes(idx_safest);
+            
+            % Get the safe nodes ids
+            SafePath_idx = Node.findTheSafestPath(allNodes,SafestNode);
+
+            % Highlight all the nodes from initial state to the safest state
+            highlight(h,SafePath_idx); 
+            % Highlight all the edges leading to the safe state
+            highlight(h,SafePath_idx(2:end),SafePath_idx(1:end-1)) 
+                   
+        end
+        
+        function Pruned_idx = findUnsafeNodes(allNodes,idx, unsafeBoundaryValue)
+            
+            Pruned_idx= idx([allNodes.UnsafetyValue]>unsafeBoundaryValue);% Mark unsafe nodes Red
+            
             % Propagate pruned nodes if all children are pruned also mark parent node red
-            AllChildrenPruned_idx = [];
             while true
                 [~,AllChildrenPruned_idx] = find(histcounts([allNodes(Pruned_idx).sourceNodeID])==3);
                 
@@ -94,13 +133,26 @@ classdef Node
                     Pruned_idx = unique([Pruned_idx AllChildrenPruned_idx]);
                 end
             end
-            highlight(h,Pruned_idx,'NodeColor','r')
-            
-            % Highlight edges that lead to unsafe nodes
+        end
+        
+        function unsafeSourceTargetPairs = findUnsafeEdges(G,Pruned_idx)
+            % Find the edges that lead to unsafe nodes
             unsafeEdges_idx=ismember(G.Edges.EndNodes(:,2), Pruned_idx);
             unsafeSourceTargetPairs =G.Edges.EndNodes(unsafeEdges_idx,:);
-            
-            highlight(h,unsafeSourceTargetPairs(:,1),unsafeSourceTargetPairs(:,2),'EdgeColor','r');
+        end
+        
+        function SafePath_idx = findTheSafestPath(allNodes,SafestNode)
+            % From the safest leaf node get the path back to the initial state
+            SafePath_idx = SafestNode.id;
+            while true
+                idx_safe = SafestNode.sourceNodeID;
+                SafestNode = allNodes(idx_safe);
+                SafePath_idx = [SafePath_idx SafestNode.id];
+                
+                if isempty(SafestNode.sourceNodeID)
+                    break;
+                end
+            end
             
         end
     end
--- ManeuverPlanning/POMDP/Pomdp.m
+++ ManeuverPlanning/POMDP/Pomdp.m
@@ -1,27 +1,38 @@
+%% Test/Tutorial script
+%This is a script for building a tree using "dummy" reachability functions 
+%for expanding states and calculating next states in some finite time horizon.
+
+% Clear all variables and load a saved snapshot from a traffic situation/scene
 clear all;
 load('decisionInputData.mat');
-
+% Just for renaming an object from the saved data Planner <- obj
 Planner = obj;
 clear obj;
 
-% Manipulate the original variables
-currentState_Ego.speed = 4;
+counter(0); % initialize the counter to later count the states in the tree
+maxDepth = 4; % Expand until nth depth if not pruned 
+deltaT = 2; % time horizon for each depth: deltaT seconds
+unsafeBoundaryValue = 0.02;
+
+% Manipulate the original variables to create meaning situation because the
+% ego vehicle is too fast and avoids collision by default
+currentState_Ego.speed = 4; % Speed reduced from 8 to 4
 
 % Determine available ego actions
-counter(0);
-Maneuvers = {'FD','EB','LC'};
+Maneuvers = {'FD','EB','LC'}; % TODO Later: create a "maneuver class" to also contain the reachability formulations
 
-UnSafetyValue=0;
-
+%% Tree Generation
+% Start by creating the root node
 rootNodes = [];
-%% POMDP Generation
-% Create root node
+UnSafetyValue=0; % Initial state for decision making shouldn't be a collision state
+
+% Create the root node but since it is the only node, it should be first a
+% leaf node so that we expand it.
 leafNode = Node([],[],currentState_Ego,Maneuvers,UnSafetyValue);
+leafNodes = leafNode;
 
-leafNodes = [leafNode];
 
-deltaT = 2; % horizon 2 seconds
-for k = 1:4
+for depth = 1:maxDepth
     
     
     
@@ -29,7 +40,7 @@ for k = 1:4
         
         newleafNodes = [];
         %expand the root node for each maneuever if safe
-        if leafNode.UnsafetyValue < 0.02
+        if leafNode.UnsafetyValue < unsafeBoundaryValue
             for maneuver = Maneuvers
                 
                 
@@ -42,9 +53,9 @@ for k = 1:4
                 for otherVehicle =1:length(currentStates_Other)
                     
                     % Calculate PDF of Other Vehicles
-                    pdf_other = calculatePDFofOtherVehicles(currentStates_Other(otherVehicle),deltaT,k);
+                    pdf_other = calculatePDFofOtherVehicles(currentStates_Other(otherVehicle),deltaT,depth);
                     % UnsafetyValue = Area under curve
-                    if abs(newleafNode.state.d - currentStates_Other(otherVehicle).d) < 0.02 % Tolerance value for "d"
+                    if abs(newleafNode.state.d - currentStates_Other(otherVehicle).d) < 0.03 % Tolerance value for "d"
                         UnSafetyValue_new = abs(pdf_other.cdf(newleafNode.state.s+2)-pdf_other.cdf(newleafNode.state.s-2)); % Size +-2 meters from the center
                         UnSafetyValue = [UnSafetyValue UnSafetyValue_new];
                     else
@@ -77,7 +88,7 @@ end
 
 allNodes = [rootNodes leafNodes];
 
-Node.plotTree(allNodes);
+Node.plotTree(allNodes,leafNodes,unsafeBoundaryValue);
 
 
 
