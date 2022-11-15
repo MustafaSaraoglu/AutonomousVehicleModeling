e2efc886b791dad1227e55b332c050d99de36545
JiangHeng <tinytinywell@gmail.com>
2022-11-15 21:39:39 +0100

    plot function and min TTC
    1. Plot function added to 'simulate_and_plot.m', relative distance, speed and acceleration can be plotted based on signals logged on 'Data Inspector'.
    2. min TTC can be calculated with 'simulate_and_plot.m'

--- simulate_and_plot.m
+++ simulate_and_plot.m
@@ -40,6 +40,59 @@ control_behavior.set('Value', controller_behavior_MathWorksMPC);
 % set the initial speed and position for Vehicle Model 2
 set_param('VehicleFollowing/Vehicle Model 2 - Following', 'Speed', ego.speed);
 set_param('VehicleFollowing/Vehicle Model 2 - Following', 'Pos', ego.position);
+%% data logging
+Simulink.sdi.markSignalForStreaming('VehicleFollowing/Vehicle Model 1 - Leader', 2, 'on');  % leader speed
+Simulink.sdi.markSignalForStreaming('VehicleFollowing/Vehicle Model 2 - Following', 3, 'on'); % ego vehicle speed
+Simulink.sdi.markSignalForStreaming('VehicleFollowing/Vehicle Model 2 - Following/Multiport Switch', 1, 'on'); % ego vehicle acceleration
+Simulink.sdi.markSignalForStreaming('VehicleFollowing/Sum2', 1, 'on'); % relative distance
 %% run the simulation
 sim('VehicleFollowing');
+%% plot
+all_data = Simulink.sdi.Run.getLatest;
+% get the data ID from Simulation data inspector to plot, the index should
+% be changed according to the corresponding index in Simulation data inspector 
+relative_distance_ID = getSignalIDByIndex(all_data, 1);
+leader_speed_ID = getSignalIDByIndex(all_data, 2);
+ego_speed_ID = getSignalIDByIndex(all_data, 3);
+acceleration_ID = getSignalIDByIndex(all_data, 4);
+% get the data according to ID
+relative_distance = Simulink.sdi.getSignal(relative_distance_ID);
+leader_speed = Simulink.sdi.getSignal(leader_speed_ID);
+ego_speed = Simulink.sdi.getSignal(ego_speed_ID);
+acceleration = Simulink.sdi.getSignal(acceleration_ID);
+% calculate min TTC
+relative_speed = leader_speed.Values.Data - ego_speed.Values.Data;
+idx = relative_speed < 0;
+TTC = -relative_distance.Values.Data(idx)./relative_speed(idx);
+min_TTC = min(TTC);
+disp("min TTC is " + num2str(min_TTC) + 's');
+% plot relative distance MathWorks
+figure;
+plot(relative_distance.Values.Time, 10 + ego_speed.Values.Data * 1.4);
+hold on;
+ylim([0, 80]);
+plot(relative_distance.Values.Time, relative_distance.Values.Data);
+hold off;
+ylabel(' ');
+legend('desired relative distance', 'actual relative distance', 'FontSize', 20);
+title('relative distance', 'fontname', 'Times New Roman', 'Color', 'k', 'FontSize', 20);
+% plot speed and acceleration
+figure;
+% speed
+subplot(1, 2, 1);
+plot(leader_speed.Values.Time, leader_speed.Values.Data);
+hold on;
+plot(ego_speed.Values.Time, ego_speed.Values.Data);
+hold off;
+ylabel(' ');
+legend('lead vehicle speed', 'ego vehicle speed', 'FontSize', 20);
+title('speed', 'fontname', 'Times New Roman', 'Color', 'k', 'FontSize', 20);
+% acceleration
+subplot(1, 2, 2);
+plot(acceleration.Values.Time, acceleration.Values.Data);
+hold off;
+ylabel(' ');
+legend('ego vehicle acceleration', 'FontSize', 20);
+ylim([-6, 3]);
+title('acceleration', 'fontname', 'Times New Roman', 'Color', 'k', 'FontSize', 20);
 end
